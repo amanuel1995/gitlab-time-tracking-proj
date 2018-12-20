6003758d389d821b3cedca22170358291ef4b421
Amanuel Goshu <amanuel.goshu@peach.matrix.msu.edu>
2018-12-20 16:37:49 -0500

    sample parser

--- parser.py
+++ parser.py
@@ -1,15 +1,15 @@
-from parsy import string, regex
+from parsy import string, regex, seq
 
 addsub = regex('added|subtracted').desc('subtract or add ')
-month = regex('([0-9]{0,2})mo').desc('Month in [0-9]mo format')
-week = regex('([0-9]{0,2})w').desc('Week in [0-9]w format')
-day = regex('([0-9]{0,2})d').desc('Day in [0-9]d format')
-hrs = regex('[0-9]{0,2}h').desc('hrs in [0-9]+h format')
-mins = regex('[0-9]{0,}m').desc('mins in [0-9]+m format')
-user = regex('[a-z]{1,}.[a-z]{1,}').desc('User in [a-z]+.[a-z]+ format')
+month = regex('([0-9]{0,2}mo)?').desc('Month in [0-9]mo format')
+week = regex('([0-9]{0,2}w)?').desc('Week in [0-9]w format')
+day = regex('([0-9]{0,2}d)?').desc('Day in [0-9]d format')
+hrs = regex('([0-9]{0,2}h)?').desc('hrs in [0-9]+h format')
+mins = regex('([0-9]{0,}m)?').desc('mins in [0-9]+m format')
+user = regex('[a-z]{1,}.[a-z]{1,}').desc('User in [a-z]{1,}.[a-z]{1,} format')
 date = regex('[0-9]{4}-[0-9]{2}-[0-9]{2}').desc('Date in yyyy-mm-dd format')
 
-parser = addsub + hrs + mins 
+parser = seq(addsub, month, week, day, hrs, mins, user, date)
 
-parser.parse('added45m')
+# parser.parse('addedamanuel.goshu2018-12-19')
 
